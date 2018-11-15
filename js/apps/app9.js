6e2309176b116b311734a8ed30ceff64482940d6
nickvaldev <nickval.dev@gmail.com>
2018-11-15 14:56:19 +0200

    some fixes

--- js/apps/app9.js
+++ js/apps/app9.js
@@ -1,4 +1,4 @@
-var https = require('https');
+// var https = require('https');
 
 
 var JsonPass = passJson;
@@ -30,49 +30,49 @@ for (let i = 0; i < JsonPassKeys.length; i++) {
 
 
 
-function convertCurrency(amount, fromCurrency, toCurrency, cb) {
-  // var apiKey = 'your-api-key-here';
+// function convertCurrency(amount, fromCurrency, toCurrency, cb) {
+//   // var apiKey = 'your-api-key-here';
 
-  fromCurrency = encodeURIComponent(fromCurrency);
-  toCurrency = encodeURIComponent(toCurrency);
-  var query = fromCurrency + '_' + toCurrency;
+//   fromCurrency = encodeURIComponent(fromCurrency);
+//   toCurrency = encodeURIComponent(toCurrency);
+//   var query = fromCurrency + '_' + toCurrency;
 
-  var url = 'https://free.currencyconverterapi.com/api/v6/convert?q='
-            + query + '&compact=ultra';
+//   var url = 'https://free.currencyconverterapi.com/api/v6/convert?q='
+//             + query + '&compact=ultra';
 
-  https.get(url, function(res){
-      var body = '';
+//   https.get(url, function(res){
+//       var body = '';
 
-      res.on('data', function(chunk){
-          body += chunk;
-      });
+//       res.on('data', function(chunk){
+//           body += chunk;
+//       });
 
-      res.on('end', function(){
-          try {
-            var jsonObj = JSON.parse(body);
+//       res.on('end', function(){
+//           try {
+//             var jsonObj = JSON.parse(body);
 
-            var val = jsonObj[query];
-            if (val) {
-              var total = val * amount;
-              cb(null, Math.round(total * 100) / 100);
-            } else {
-              var err = new Error("Value not found for " + query);
-              console.log(err);
-              cb(err);
-            }
-          } catch(e) {
-            console.log("Parse error: ", e);
-            cb(e);
-          }
-      });
-  }).on('error', function(e){
-        console.log("Got an error: ", e);
-        cb(e);
-  });
-}
+//             var val = jsonObj[query];
+//             if (val) {
+//               var total = val * amount;
+//               cb(null, Math.round(total * 100) / 100);
+//             } else {
+//               var err = new Error("Value not found for " + query);
+//               console.log(err);
+//               cb(err);
+//             }
+//           } catch(e) {
+//             console.log("Parse error: ", e);
+//             cb(e);
+//           }
+//       });
+//   }).on('error', function(e){
+//         console.log("Got an error: ", e);
+//         cb(e);
+//   });
+// }
 
 //uncomment to test
 
-convertCurrency(10, 'USD', 'PHP', function(err, amount) {
-  console.log(amount);
-});
+// convertCurrency(10, 'USD', 'PHP', function(err, amount) {
+//   console.log(amount);
+// });
