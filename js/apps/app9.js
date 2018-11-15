3b7b27decf824a2d2d15e435384d2ef080679a1c
nickvaldev <nickval.dev@gmail.com>
2018-11-15 17:48:21 +0200

    some fixes

--- js/apps/app9.js
+++ js/apps/app9.js
@@ -1,6 +1,3 @@
-// var https = require('https');
-
-
 var JsonPass = passJson;
 JsonPassKeys = Object.keys(JsonPass);
 // console.log(JsonPassKeys[0]);
@@ -12,14 +9,12 @@ for (let i = 0; i < JsonPassKeys.length; i++) {
   df.appendChild(option);
   var select1 = document.getElementById("app9_select_id_0");
   select1.appendChild(df);
-  var select2 = document.getElementById("app9_select_id_1");
-  select2.appendChild(df);
 }
 
 for (let i = 0; i < JsonPassKeys.length; i++) {
   let df = document.createDocumentFragment();
   var option = document.createElement("option");
-  option.innerHTML = JsonPass[JsonPassKeys[i]].currencyName;
+  option.innerHTML = JsonPass[JsonPassKeys[i]].currencyName + ' - ' + JsonPass[JsonPassKeys[i]].id;
   option.value = JsonPass[JsonPassKeys[i]].id;
   df.appendChild(option);
   var select2 = document.getElementById("app9_select_id_1");
@@ -28,51 +23,42 @@ for (let i = 0; i < JsonPassKeys.length; i++) {
 
 
 
+var fromEl = document.getElementById('app9_select_id_0');
+
+var toEl = document.getElementById('app9_select_id_1');
 
 
-// function convertCurrency(amount, fromCurrency, toCurrency, cb) {
-//   // var apiKey = 'your-api-key-here';
 
-//   fromCurrency = encodeURIComponent(fromCurrency);
-//   toCurrency = encodeURIComponent(toCurrency);
-//   var query = fromCurrency + '_' + toCurrency;
 
-//   var url = 'https://free.currencyconverterapi.com/api/v6/convert?q='
-//             + query + '&compact=ultra';
+document.getElementById('app9_form').addEventListener('submit', app9_calculateResults);
 
-//   https.get(url, function(res){
-//       var body = '';
 
-//       res.on('data', function(chunk){
-//           body += chunk;
-//       });
+function app9_calculateResults(e) {
+  var input = document.getElementById('app9_input_id_0').value;
+  var fromValue = fromEl.options[fromEl.selectedIndex].value;
+  var toValue = toEl.options[toEl.selectedIndex].value;
+  var fromDols = JsonPass[fromValue].dol;
+  var toDols = JsonPass[toValue].dol;
+  var result = (toDols / fromDols) * input ;
+  if (typeof JsonPass[fromValue].currencySymbol !== 'undefined') {
+    document.getElementById('app9_input_span_id_0').textContent = JsonPass[fromValue].currencySymbol;
+    document.getElementById('app9_results_span_content_sign_0').textContent = JsonPass[fromValue].currencySymbol;
+  } else {
+    document.getElementById('app9_input_span_id_0').innerHTML = JsonPass[fromValue].id;
+    document.getElementById('app9_results_span_content_sign_0').innerHTML = JsonPass[fromValue].id;
+  }
 
-//       res.on('end', function(){
-//           try {
-//             var jsonObj = JSON.parse(body);
+  if (typeof JsonPass[toValue].currencySymbol !== 'undefined') { 
+    document.getElementById('app9_results_span_content_sign_1').textContent = JsonPass[toValue].currencySymbol;
+  } else {
+    document.getElementById('app9_results_span_content_sign_1').innerHTML = JsonPass[toValue].id;
+  }
+  
+  document.getElementById('app9_results_title_0').innerHTML = JsonPass[fromValue].currencyName + ' - ' + JsonPass[fromValue].id;
+  document.getElementById('app9_results_title_1').innerHTML = JsonPass[toValue].currencyName + ' - ' + JsonPass[toValue].id;
+  document.getElementById('app9_results_span_content_0').textContent = input;
 
-//             var val = jsonObj[query];
-//             if (val) {
-//               var total = val * amount;
-//               cb(null, Math.round(total * 100) / 100);
-//             } else {
-//               var err = new Error("Value not found for " + query);
-//               console.log(err);
-//               cb(err);
-//             }
-//           } catch(e) {
-//             console.log("Parse error: ", e);
-//             cb(e);
-//           }
-//       });
-//   }).on('error', function(e){
-//         console.log("Got an error: ", e);
-//         cb(e);
-//   });
-// }
+  document.getElementById('app9_results_span_content_1').textContent = result;
 
-//uncomment to test
-
-// convertCurrency(10, 'USD', 'PHP', function(err, amount) {
-//   console.log(amount);
-// });
+  e.preventDefault();
+}
